09e55dcb142e4bee381a4140cc23f92cfe6f3620
Khaled mubayid <khaledmubayid@hotmail.com>
2022-07-20 15:27:25 +0300

    Create IRepository Interface For Data base opreation

---
+++ MarketplacesIntegration.Core/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketplacesIntegration.Core
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+
+    }
+}
