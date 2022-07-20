ebd021b9589ea599a4e4ef21e59e28464f19da54
Khaled mubayid <khaledmubayid@hotmail.com>
2022-07-20 15:52:25 +0300

    Add Interface IRepository to class Repository

--- MarketplacesIntegration.Service/Repositories/Repository.cs
+++ MarketplacesIntegration.Service/Repositories/Repository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MarketplacesIntegration.Core.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace MarketplacesIntegration.Service.Repositories
 {
-    public class Repository
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
     }
 }
